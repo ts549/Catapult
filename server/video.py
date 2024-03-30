d4536aecc30b26cc5d04f8cbd4cdf7c674ddbce3
Sean Lee <65737799+seeenlee@users.noreply.github.com>
2024-03-30 13:47:05 -0400

    changed variable names in video.py

--- server/video.py
+++ server/video.py
@@ -3,11 +3,12 @@ from moviepy.editor import VideoFileClip
 
 model = whisper.load_model("base")
 
-def video_to_audio(input_file, output_file):
+def split_video_audio(input_file, video_path, audio_path):
   video = VideoFileClip(input_file)
-  video.audio.write_audiofile(output_file)
+  video.without_audio().write_videofile(video_path)
+  video.audio.write_audiofile(audio_path)
   
-def parse_video(input_file):
+def transcribe_audio(input_file):
   result = model.transcribe(input_file)
-  return result
+  return result["text"]
 
