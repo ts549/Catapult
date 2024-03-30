c6b237a7b5f4321044f98ef2e32e6c55e23e3011
Kush Khanna <57548935+krash3125@users.noreply.github.com>
2024-03-30 14:51:31 -0400

    push

---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-center items-center">
+      Hello
+    </main>
+  );
+}
