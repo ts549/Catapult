f0f3a91f4a13b0f6b3f1dc5dde6331791d23cfb6
Kush Khanna <57548935+krash3125@users.noreply.github.com>
2024-03-31 00:23:00 -0400

    push

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,34 +1,31 @@
 import './Navbar.css';
-import { NavLink } from '@mantine/core';
+import { Group, NavLink, Title } from '@mantine/core';
 import { IconHome2, IconGauge, IconSettings } from '@tabler/icons-react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 function Navbar() {
   return (
-    <div className="w-[20%] h-[100%] bg-[#ababab] flex flex-col items-center">
-      <div
-        id="logo"
-        className="w-[80%] h-[10%] mt-[30px] flex flex-row gap-3 items-center"
-      >
-        <div className="w-[60px] h-[40px] bg-white" />
-        <div className="w-[200px] h-[30px] p-1 bg-white">logo in text form</div>
-      </div>
+    <div className="text-white w-[25%] h-[100%] bg-[#1f1f1f] flex flex-col px-4 py-8">
+      <Group className="mx-auto" gap="sm" mb={60}>
+        <Image src="/logo.png" alt="logo" width={36} height={36} />
+        <Title className="" order={3}>
+          Acumentor
+        </Title>
+      </Group>
 
       <Link href="/home" className="w-full">
         <NavLink
           label="Home"
-          pl={40}
           leftSection={<IconHome2 size="1.25rem" stroke={1.5} />}
         />
       </Link>
       <NavLink
         label="Profile"
-        pl={40}
         leftSection={<IconGauge size="1.25rem" stroke={1.5} />}
       />
       <NavLink
         label="Settings"
-        pl={40}
         leftSection={<IconSettings size="1.25rem" stroke={1.5} />}
       />
     </div>
