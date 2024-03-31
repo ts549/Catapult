91b7e56ce9e2d352a982d0b5c536c992b70fb657
Sean Lee <65737799+seeenlee@users.noreply.github.com>
2024-03-30 22:28:53 -0400

    openai api

--- server/api.py
+++ server/api.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask, flash, request, redirect, url_for, jsonify, make_response 
 from werkzeug.exceptions import BadRequest
 from video import split_video_audio, transcribe_audio, save_file
+from gpt import create_quiz
 import sys
 from flask_cors import CORS
+import requests
 
 
 
@@ -24,11 +26,11 @@ def upload_file():
     print("DONE SPLITTING")
     transcription = transcribe_audio(audio_path)
     print("DONE TRANSCRIBING")
-    
     parts = file['path'].split('/')
     id = parts[2]
-    data = {'id': id, 'status': 'Success', 'message': 'File saved', 'video_path': file['path'], "audio_path": audio_path, "transcription": transcription, "file_name": file['name'] }
-    
+    questions = create_quiz(request.form["multiple_choice"], request.form["true_false"], request.form["short_answer"], request.form["variations"])
+    print("DONE QUESTIONS")
+    data = {'id': id, 'status': 'Success', 'message': 'File saved', 'video_path': file_path, "audio_path": audio_path, "transcription": transcription, "questions": questions}
     return make_response(jsonify(data), 200)
 
 
---
+++ server/gpt.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+from flask import jsonify
+import pickle
+load_dotenv()
+
+client = OpenAI()
+
+SYSTEM_STRING = "You are a quiz generator that generates quiz questions and answers for users. It is very important that the format you answer with is valid JSON."
+
+FORMAT_STRING="""
+The format of your response should be a valid JSON that looks like this:
+interface Question {
+  question: string;
+  choices?: string[];
+  answer?: string;
+  question_type: "MULTIPLE_CHOICE" | "TRUE_FALSE" | "SHORT_ANSWER";
+}[]
+"""
+
+def openai_call(prompt, variations):
+    response = client.chat.completions.create(model="gpt-3.5-turbo-0125",
+                                              n=variations,
+                                              response_format={ "type": "json_object" },
+                                              messages=[
+                                                  {"role": "system", "content": SYSTEM_STRING + FORMAT_STRING},
+                                                  {"role": "user", "content": prompt}
+                                              ])
+    return [choice.message.content for choice in response.choices]
+
+def create_quiz(multiple_choice, true_false, short_answer, variations):
+    string = f"Create a quiz that has {multiple_choice} multiple choice questions, {true_false} true and false questions, and {short_answer} short_answer questions"
+    return openai_call(string, int(variations))
